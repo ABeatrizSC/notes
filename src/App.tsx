875c4b7a2681ddbdfc5a01a2ed364dd9d334c2b2
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-02-06 23:44:06 -0300

    Implementar funcionalidades nos componentes new-note-card e note-card e utilizar as bibliotecas Radix UI, date-fns e Sonner

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,10 @@ export function App() {
 
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
         <NewNoteCard/>
-        <NoteCard />
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World'
+          }} />
       </div>
     </div>
   )
